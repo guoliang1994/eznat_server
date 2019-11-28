bf66a2040830269cf2e32961a66cbe8561d721ec
九九一十八 <761297884@qq.com>
2019-11-28 18:38:46 +0800

    服务端使用自带的php运行

--- eznat/Manage.php
+++ eznat/Manage.php
@@ -1,39 +1,46 @@
 <?php
 namespace core;
+
 class Manage
 {
+    private static $phpPath = "../runenv/linux_php/bin/php";
     public static function start()
     {
         $scriptFile = __DIR__ . "/server.php";
-        exec("php {$scriptFile} start -d", $out);
+        $php = self::$phpPath;
+        exec("{$php} {$scriptFile} start -d", $out);
         return $out;
     }
 
     public static function restart()
     {
         $scriptFile = __DIR__ . "/server.php";
-        exec("php {$scriptFile} restart -d", $out);
+        $php = self::$phpPath;
+        exec("{$php} {$scriptFile} restart -d", $out);
         return $out;
     }
 
     public static function stop()
     {
         $scriptFile = __DIR__ . "/server.php";
-        exec("php {$scriptFile} stop", $out);
+        $php = self::$phpPath;
+        exec("{$php} {$scriptFile} stop", $out);
         return $out;
     }
 
     public static function reload()
     {
         $scriptFile = __DIR__ . "/server.php";
-        exec("php  {$scriptFile} reload", $out);
+        $php = self::$phpPath;
+        exec("{$php}  {$scriptFile} reload", $out);
         return $out;
     }
 
     public static function status()
     {
         $scriptFile = __DIR__ . "/server.php";
-        exec("php  {$scriptFile} status", $out);
+        $php = self::$phpPath;
+        exec("{$php}  {$scriptFile} status", $out);
         return $out;
     }
 }
